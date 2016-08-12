4954fa6e08896baf8dfb01e45e789168ef0bc7b6
Adam Greig <adam@adamgreig.com>
2016-08-12 04:58:25 +0100

    improve sp100 driver

--- m3dl/extpressure/firmware/get_values.sh
+++ m3dl/extpressure/firmware/get_values.sh
@@ -3,7 +3,7 @@ arm-none-eabi-gdb --batch --quiet \
                     -ex 'file build/extpressurefw.elf' \
                     -ex 'monitor swdp_scan' \
                     -ex 'attach 1' \
-                    -ex 'printf "ID=%d T=%dC B=%.02fV A=%.1fg P=%.01fkPa\n", sp100s[0].id, sp100s[0].temperature - 50, (double)sp100s[0].battery * 0.0184 + 1.73, (double)sp100s[0].acceleration * 0.5 - 12, (double)sp100s[0].pressure * 1.25'\
-                    -ex 'printf "ID=%d T=%dC B=%.02fV A=%.1fg P=%.01fkPa\n", sp100s[1].id, sp100s[1].temperature - 50, (double)sp100s[1].battery * 0.0184 + 1.73, (double)sp100s[1].acceleration * 0.5 - 12, (double)sp100s[1].pressure * 1.25'\
-                    -ex 'printf "ID=%d T=%dC B=%.02fV A=%.1fg P=%.01fkPa\n", sp100s[2].id, sp100s[2].temperature - 50, (double)sp100s[2].battery * 0.0184 + 1.73, (double)sp100s[2].acceleration * 0.5 - 12, (double)sp100s[2].pressure * 1.25'\
-                    -ex 'printf "ID=%d T=%dC B=%.02fV A=%.1fg P=%.01fkPa\n", sp100s[3].id, sp100s[3].temperature - 50, (double)sp100s[3].battery * 0.0184 + 1.73, (double)sp100s[3].acceleration * 0.5 - 12, (double)sp100s[3].pressure * 1.25'\
+                    -ex 'printf "ID=%d T=%dC B=%.02fV A=%+.01fg P=%.01fkPa\n", sp100s[0].id, sp100s[0].temperature - 50, (double)sp100s[0].battery * 0.0184 + 1.73, (double)sp100s[0].acceleration * 0.5 - 12, (double)sp100s[0].pressure * 1.25'\
+                    -ex 'printf "ID=%d T=%dC B=%.02fV A=%+.01fg P=%.01fkPa\n", sp100s[1].id, sp100s[1].temperature - 50, (double)sp100s[1].battery * 0.0184 + 1.73, (double)sp100s[1].acceleration * 0.5 - 12, (double)sp100s[1].pressure * 1.25'\
+                    -ex 'printf "ID=%d T=%dC B=%.02fV A=%+.01fg P=%.01fkPa\n", sp100s[2].id, sp100s[2].temperature - 50, (double)sp100s[2].battery * 0.0184 + 1.73, (double)sp100s[2].acceleration * 0.5 - 12, (double)sp100s[2].pressure * 1.25'\
+                    -ex 'printf "ID=%d T=%dC B=%.02fV A=%+.01fg P=%.01fkPa\n", sp100s[3].id, sp100s[3].temperature - 50, (double)sp100s[3].battery * 0.0184 + 1.73, (double)sp100s[3].acceleration * 0.5 - 12, (double)sp100s[3].pressure * 1.25'\
--- m3dl/extpressure/firmware/sp100.c
+++ m3dl/extpressure/firmware/sp100.c
@@ -25,6 +25,15 @@
 #define SP100_CMD_SETWK         0x2E
 #define SP100_CMD_GETWK         0x2F
 
+#define SP100_STATUS_OVERFLOW   (1<<0)
+#define SP100_STATUS_PARITY     (1<<1)
+#define SP100_STATUS_CHECKSUM   (1<<2)
+#define SP100_STATUS_PROM       (1<<3)
+#define SP100_STATUS_LOWV       (1<<4)
+#define SP100_STATUS_FAULT      (1<<5)
+#define SP100_STATUS_ACCEL      (1<<6)
+#define SP100_STATUS_MEASURE    (1<<7)
+
 static SPIDriver* sp100_spid;
 static ioline_t* sp100_ss;
 static size_t sp100_nss;
@@ -32,7 +41,7 @@ static struct SP100* sp100_out;
 
 static void sp100_spi_start(int n);
 static void sp100_spi_stop(void);
-static void sp100_reset(int n);
+static void sp100_reload(int n);
 static void sp100_acceleration_measure(int n);
 static void sp100_acceleration_read(int n);
 static void sp100_temperature_measure(int n);
@@ -45,6 +54,8 @@ static void sp100_id(int n);
 static uint8_t sp100_status(int n);
 static uint8_t sp100_read_byte(int n, uint8_t cmd);
 static void sp100_send_cmd(int n, uint8_t cmd);
+static void sp100_wait_measurement(int n);
+static void sp100_check_status(int n);
 
 static SPIConfig spi_cfg = {
     .end_cb = NULL,
@@ -63,7 +74,7 @@ static void sp100_spi_stop() {
     spiStop(sp100_spid);
 }
 
-static void sp100_reset(int n) {
+static void sp100_reload(int n) {
     sp100_send_cmd(n, SP100_CMD_LDOTP);
 }
 
@@ -149,63 +160,84 @@ static void sp100_send_cmd(int n, uint8_t cmd) {
     sp100_spi_stop();
 }
 
+static void sp100_wait_measurement(int n) {
+    /* TODO Signal the error over serial too */
+
+    systime_t start_time = chVTGetSystemTime();
+
+    while(sp100_status(n) & SP100_STATUS_MEASURE) {
+        /* If we've been waiting more than 50ms, signal an error and
+         * continue anyway.
+         */
+        if(ST2MS(chVTGetSystemTime() - start_time) > 50) {
+            palSetLine(LINE_LED_RED);
+            return;
+        }
+    }
+}
+
+static void sp100_check_status(int n) {
+    /* TODO Signal the error over serial too */
+    if(sp100_status(n) & (SP100_STATUS_PARITY   |
+                          SP100_STATUS_CHECKSUM |
+                          SP100_STATUS_LOWV     |
+                          SP100_STATUS_FAULT))
+    {
+        palSetLine(LINE_LED_RED);
+    }
+}
+
 static THD_WORKING_AREA(sp100_thd_wa, 256);
 static THD_FUNCTION(sp100_thd, arg) {
     (void)arg;
     size_t i;
 
-    /* Wait for SP100 10ms POR */
-    chThdSleepMilliseconds(12);
+    /* Wait for SP100 POR */
+    chThdSleepMilliseconds(50);
 
-    /* Force reset of all pressure sensors */
+    /* Force reload of OTP of all pressure sensors */
     for(i=0; i<sp100_nss; i++) {
-        sp100_reset(i);
+        sp100_reload(i);
+        /* Wait until PROM reload bit is cleared */
+        while(sp100_status(i) & SP100_STATUS_PROM);
+        /* Also wait until status returns all 0s (ignoring accel direction) */
+        while(sp100_status(i) & ~SP100_STATUS_ACCEL);
     }
 
-    /* Wait for reset to be done */
-    chThdSleepMilliseconds(10);
+    /* Wait for reload to be done */
+    chThdSleepMilliseconds(50);
 
-    /* Read all IDs */
+    /* Read all IDs and supply voltages and accelerations */
+    /* Read battery twice to warm up the ADC */
     for(i=0; i<sp100_nss; i++) {
+        sp100_battery_measure(i);
+        sp100_wait_measurement(i);
+        sp100_battery_measure(i);
+        sp100_wait_measurement(i);
+        sp100_battery_read(i);
+        sp100_acceleration_measure(i);
+        sp100_wait_measurement(i);
+        sp100_acceleration_read(i);
         sp100_id(i);
     }
 
     /* Go around the loop reading sensors */
     while(true) {
         for(i=0; i<sp100_nss; i++) {
-            volatile uint8_t status;
-
-            sp100_id(i);
-            chThdSleepMilliseconds(1);
-
-            sp100_battery_measure(i);
-            chThdSleepMilliseconds(20);
-            status = sp100_status(i);
-            sp100_battery_read(i);
-            chThdSleepMilliseconds(1);
-
+            /* Read temperature */
             sp100_temperature_measure(i);
-            chThdSleepMilliseconds(20);
-            status = sp100_status(i);
+            sp100_wait_measurement(i);
             sp100_temperature_read(i);
-            chThdSleepMilliseconds(1);
+            sp100_check_status(i);
 
-            sp100_acceleration_measure(i);
-            chThdSleepMilliseconds(20);
-            status = sp100_status(i);
-            sp100_acceleration_read(i);
-            chThdSleepMilliseconds(1);
-
+            /* Read pressure */
             sp100_pressure_measure(i);
-            chThdSleepMilliseconds(20);
-            status = sp100_status(i);
+            sp100_wait_measurement(i);
             sp100_pressure_read(i);
-            chThdSleepMilliseconds(1);
+            sp100_check_status(i);
 
-            (void)status;
-
-            chThdSleepMilliseconds(100);
         }
+        /* TODO Signal completed read */
     }
 }
 
