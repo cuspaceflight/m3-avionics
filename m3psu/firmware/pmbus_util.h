853a3525388fe7803e82178b5842d16b11073b24
JMW95 <jamie.wood70@ntlworld.com>
2016-08-15 18:15:55 +0100

    Update Makefile to new template, fix build issues

--- m3psu/firmware/pmbus_util.h
+++ m3psu/firmware/pmbus_util.h
@@ -5,16 +5,14 @@
 #ifndef PMBUS_UTIL_H_
 #define PMBUS_UTIL_H_
 
-#include <math.h>
-
 float L16_to_float(int8_t exp, uint16_t input_val){
   int16_t mantissa = input_val;
 
-  return mantissa * pow(2, exp);
+  return mantissa * (1<<exp);
 }
 
 uint16_t float_to_L16(int8_t exp, float input_val){
-  float exponent = pow(2, exp);
+  float exponent = (1<<exp);
   return (uint16_t)(input_val / exponent);
 }
 
