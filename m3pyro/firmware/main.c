bd33d2c8974a6445acc46ff84571bb3ead95ed28
Adam Greig <adam@adamgreig.com>
2016-08-23 18:58:37 +0100

    tickless mode + sleep

--- m3pyro/firmware/main.c
+++ m3pyro/firmware/main.c
@@ -5,22 +5,21 @@
 #include "m3pyro_continuity.h"
 
 int main(void) {
+    halInit();
+    chSysInit();
 
-      halInit();
-      chSysInit();
+    can_init();
 
-      can_init();
+    palClearLine(LINE_FIRE1);
+    palClearLine(LINE_FIRE2);
+    palClearLine(LINE_FIRE3);
+    palClearLine(LINE_FIRE4);
 
-      palClearLine(LINE_FIRE1);
-      palClearLine(LINE_FIRE2);
-      palClearLine(LINE_FIRE3);
-      palClearLine(LINE_FIRE4);
+    m3pyro_continuity_init();
 
-      m3pyro_continuity_init();
-
-      while (true) {
-          chThdSleep(TIME_INFINITE);
-      }
+    while (true) {
+        chThdSleep(TIME_INFINITE);
+    }
 }
 
 void can_recv(uint16_t msg_id, bool can_rtr, uint8_t *data, uint8_t datalen) {
