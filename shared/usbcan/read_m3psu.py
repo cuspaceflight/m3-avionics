c6cd714972b0647140562c4f7cd9da5f219fb9de
Adam Greig <adam@adamgreig.com>
2016-08-24 12:04:11 +0100

    update read_m3psu commands as well, oops

--- shared/usbcan/read_m3psu.py
+++ shared/usbcan/read_m3psu.py
@@ -4,6 +4,24 @@ from queue import Empty
 
 import usbcan
 
+
+m3psu_did = 0b00010
+msg_id = lambda x: x << 5
+m3psu_msg_toggle_pyros = m3psu_did | msg_id(16)
+m3psu_msg_toggle_channel = m3psu_did | msg_id(17)
+m3psu_msg_toggle_charger = m3psu_did | msg_id(18)
+m3psu_msg_toggle_balance = m3psu_did | msg_id(19)
+m3psu_msg_pyro_status = m3psu_did | msg_id(48)
+m3psu_msg_channel_status_1_2 = m3psu_did | msg_id(49)
+m3psu_msg_channel_status_3_4 = m3psu_did | msg_id(50)
+m3psu_msg_channel_status_5_6 = m3psu_did | msg_id(51)
+m3psu_msg_channel_status_7_8 = m3psu_did | msg_id(52)
+m3psu_msg_channel_status_9_10 = m3psu_did | msg_id(53)
+m3psu_msg_channel_status_11_12 = m3psu_did | msg_id(54)
+m3psu_msg_charger_status = m3psu_did | msg_id(55)
+m3psu_msg_battery_voltages = m3psu_did | msg_id(56)
+
+
 port = sys.argv[1]
 txq = multiprocessing.Queue()
 rxq = multiprocessing.Queue()
@@ -24,32 +42,33 @@ def unpack_channels():
     
 def process_frame(frame):
     global batt, pyro, channels, charger
-    pid = frame.sid >> 5
     did = frame.sid & 0b11111
-    if did != 2:
+    if did != m3psu_did:
         return
-    if pid == 56:
+    if frame.sid == m3psu_msg_battery_voltages:
         batt[0] = (float(frame.data[0]*2)/100.0)
         batt[1] = (float(frame.data[1]*2)/100.0)
         batt[2] = (frame.data[2] & (1<<2)) != 0
         batt[3] = (frame.data[2] & (1<<1)) != 0
         batt[4] = (frame.data[2] & (1<<0)) != 0
-    elif pid == 48:
+    elif frame.sid == m3psu_msg_pyro_status:
         ints = frame.as_int16()
         pyro[0:2] = [ (float(b)/1000.0) for b in ints[0:2] ]
         pyro[2] = float(ints[2]) / 10.0
         pyro[3] = "Disabled" if ints[3] == 0 else "Enabled"
-    elif pid >= 49 and pid <= 54:
+    elif (frame.sid >= m3psu_msg_channel_status_1_2
+            and frame.sid <= m3psu_msg_channel_status_11_12):
         floats = list(map(float, frame.data))
-        channels[(pid-3)*2] = [floats[0]*0.03, floats[1]*0.003, floats[2]*0.02]
-        channels[((pid-3)*2) + 1] = [floats[4]*0.03, floats[5]*0.003, floats[6]*0.02]
-    elif pid == 55:
+        first_channel_id = (frame.sid >> 5) - (m3psu_msg_channel_status_1_2 >> 5)
+        channels[first_channel_id] = [floats[0]*0.03, floats[1]*0.003, floats[2]*0.02]
+        channels[first_channel_id + 1] = [floats[4]*0.03, floats[5]*0.003, floats[6]*0.02]
+    elif frame.sid == m3psu_msg_charger_status:
         charger[0] = (frame.data[1] << 8) | frame.data[0]
         charger[1] = (frame.data[2] & (1<<2)) != 0
         charger[2] = (frame.data[2] & (1<<1)) != 0
         charger[3] = (frame.data[2] & (1<<0)) != 0
     else:
-        print("Unknown SID %d" % pid)
+        print("Unknown SID %d" % frame.sid)
 
 def inputtrigger(txq):
     while True:
@@ -61,35 +80,35 @@ def inputtrigger(txq):
                     chan = int(line[1])
                 except:
                     continue
-                packet = usbcan.CANFrame((2<<6)|9, 0, 2, [1, chan])
+                packet = usbcan.CANFrame(m3psu_msg_toggle_channel, 0, 2, [1, chan])
                 txq.put(packet)
             elif cmd == "off":
                 try:
                     chan = int(line[1])
                 except:
                     continue
-                packet = usbcan.CANFrame((2<<6)|9, 0, 2, [0, chan])
+                packet = usbcan.CANFrame(m3psu_msg_toggle_channel, 0, 2, [0, chan])
                 txq.put(packet)
             elif cmd == "pyro":
                 if line[1] == "enable":
-                    packet = usbcan.CANFrame((2<<6)|2, 0, 1, [1])
+                    packet = usbcan.CANFrame(m3psu_msg_toggle_pyros, 0, 1, [1])
                     txq.put(packet)
                 elif line[1] == "disable":
-                    packet = usbcan.CANFrame((2<<6)|2, 0, 1, [0])
+                    packet = usbcan.CANFrame(m3psu_msg_toggle_pyros, 0, 1, [0])
                     txq.put(packet)
             elif cmd == "balance":
                 if line[1] == "enable":
-                    packet = usbcan.CANFrame((2<<6)|13, 0, 1, [1])
+                    packet = usbcan.CANFrame(m3psu_msg_toggle_balance, 0, 1, [1])
                     txq.put(packet)
                 elif line[1] == "disable":
-                    packet = usbcan.CANFrame((2<<6)|13, 0, 1, [0])
+                    packet = usbcan.CANFrame(m3psu_msg_toggle_balance, 0, 1, [0])
                     txq.put(packet)
             elif cmd == "charger":
                 if line[1] == "enable":
-                    packet = usbcan.CANFrame((2<<6)|12, 0, 1, [1])
+                    packet = usbcan.CANFrame(m3psu_msg_toggle_charger, 0, 1, [1])
                     txq.put(packet)
                 elif line[1] == "disable":
-                    packet = usbcan.CANFrame((2<<6)|12, 0, 1, [0])
+                    packet = usbcan.CANFrame(m3psu_msg_toggle_charger, 0, 1, [0])
                     txq.put(packet)
 
 def mainloop(txq, rxq):
