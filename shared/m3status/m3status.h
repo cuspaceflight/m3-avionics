550f5f0c7f5f6f0370f9181177cb06501dc50838
Adam Greig <adam@adamgreig.com>
2016-08-25 18:59:44 +0100

    add functions to get status from m3status

--- shared/m3status/m3status.h
+++ shared/m3status/m3status.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
+#define M3STATUS_OK                 (0)
+#define M3STATUS_INITIALISING       (1<<0)
+#define M3STATUS_ERROR              (1<<1)
+
 /* Call to update status, with optional error code.
  * Call initialising() for each component to start including that component ID
  * and to send an initialising status message for it.
@@ -16,5 +20,11 @@
 void m3status_set_init(uint8_t component);
 void m3status_set_ok(uint8_t component);
 void m3status_set_error(uint8_t component, uint8_t errorcode);
+
+/* Get the current board's overall status */
+uint8_t m3status_get(void);
+
+/* Get an individual component's current status */
+uint8_t m3status_get_component(uint8_t component);
 
 #endif
