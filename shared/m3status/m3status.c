5a1d484757f280914aaa7d2f247fe1acfcf518db
Adam Greig <adam@adamgreig.com>
2016-09-01 00:27:28 +0100

    have m3status only retransmit status on change or after 500ms

--- shared/m3status/m3status.c
+++ shared/m3status/m3status.c
@@ -3,6 +3,7 @@
 #include "m3can.h"
 
 static uint8_t components[256] = {0};
+static systime_t last_transmit[256] = {0};
 
 static void m3status_set(uint8_t component, uint8_t status, uint8_t errorcode);
 
@@ -22,19 +23,32 @@ static void m3status_set(uint8_t component, uint8_t status, uint8_t errorcode)
 {
     chDbgAssert(m3can_own_id != 0, "m3can_init() hasn't been called");
 
+    /* Check if we should transmit the new status */
+    bool transit_status = false;
+    if(components[component] != status) {
+        /* Transmit if status has changed since last time */
+        transmit_status = true;
+    } else if (ST2MS(chVTTimeElapsedSinceX(last_transmit[component])) > 500) {
+        /* Transmit if we haven't sent this one in at least 500ms */
+        transmit_status = true;
+    }
+
     /* Store new status for this component */
     components[component] = status;
 
-    /* Compute overall status as highest of all components */
-    uint8_t overall_status = m3status_get();
+    if(transmit_status) {
+        /* Compute overall status as highest of all components */
+        uint8_t overall_status = m3status_get();
 
-    uint8_t data[4] = {overall_status, component, status, errorcode};
-    uint8_t len = 3;
-    if(status == M3STATUS_ERROR) {
-        len = 4;
+        uint8_t data[4] = {overall_status, component, status, errorcode};
+        uint8_t len = 3;
+        if(status == M3STATUS_ERROR) {
+            len = 4;
+        }
+
+        can_send(m3can_own_id | CAN_MSG_ID_STATUS, false, data, len);
+        last_transmit[component] = chVTGetSystemTimeX();
     }
-
-    can_send(m3can_own_id | CAN_MSG_ID_STATUS, false, data, len);
 }
 
 uint8_t m3status_get_component(uint8_t component) {
