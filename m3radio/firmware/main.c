0847def680d0be376ec794542458fffa56ef918a
Adam Greig <adam@adamgreig.com>
2016-09-19 04:07:58 +0100

    quick and messy example of successfully using labrador

--- m3radio/firmware/main.c
+++ m3radio/firmware/main.c
@@ -5,6 +5,55 @@
 #include "m3radio_gps_ant.h"
 #include "ublox.h"
 
+#include <string.h>
+
+#include "radios/si446x.h"
+
+THD_WORKING_AREA(tmpwa, 512);
+THD_FUNCTION(radthd, arg) {
+    (void)arg;
+    struct si446x_board_config brdcfg = {
+        .spid = &SPID2,
+        .spi_cfg = {
+            .end_cb = NULL,
+            .ssport = GPIOB,
+            .sspad = GPIOB_RADIO_CS,
+            .cr1 = SPI_CR1_BR_2,
+        },
+        .sdn = LINE_RADIO_SDN,
+        .tcxo = true,
+        .xo_freq = 26000000,
+    };
+
+    struct labrador_radio_config labcfg = {
+        .freq = 869500000,
+        .baud = 2000,
+        .txlen = 32,
+        .rxlen = 32,
+    };
+
+    uint8_t rxbuf[32] = {0};
+
+    m3status_set_init(M3RADIO_COMPONENT_SI4460);
+
+    bool rv = si446x_init(&brdcfg, &labcfg);
+
+    if(!rv) {
+        m3status_set_error(M3RADIO_COMPONENT_SI4460, M3RADIO_ERROR_SI4460_CFG);
+        while(true) {
+            chThdSleepMilliseconds(100);
+        }
+    }
+
+    while (true) {
+        m3status_set_ok(M3RADIO_COMPONENT_SI4460);
+        memset(rxbuf, 0, sizeof(rxbuf));
+        rv = si446x_rx(rxbuf);
+        chThdSleepMilliseconds(100);
+    }
+
+}
+
 int main(void) {
 
     /* Allow debug access during WFI sleep */
@@ -48,6 +97,8 @@ int main(void) {
         .centre_freq = 869500000,
     };
 #endif
+
+    chThdCreateStatic(tmpwa, sizeof(tmpwa), NORMALPRIO, radthd, NULL);
 
     while (true) {
         /* Clear the watchdog timer */
