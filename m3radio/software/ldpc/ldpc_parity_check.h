13d89cbafd1343f352a0b821a439b821d323ac97
Adam Greig <adam@adamgreig.com>
2016-09-19 02:14:56 +0100

    remove all the ldpc stuff, moved to labrador

--- m3radio/software/ldpc/ldpc_parity_check.h
+++
@@ -1,8 +0,0 @@
-#ifndef LDPC_PARITY_CHECK_H
-#define LDPC_PARITY_CHECK_H
-
-#include <stdbool.h>
-
-extern const bool ldpc_parity[128][256];
-
-#endif /* LDPC_PARITY_CHECK_H */
--- m3radio/software/ldpc/ldpc_parity_check_packed.h
+++
@@ -1,8 +0,0 @@
-#ifndef LDPC_PARITY_CHECK_PACKED_H
-#define LDPC_PARITY_CHECK_PACKED_H
-
-#include <stdint.h>
-
-extern const uint64_t ldpc_parity_p[128][4];
-
-#endif /* LDPC_PARITY_CHECK_PACKED_H */
