ad096286356d6d81b2bc8afe590c866ec1dfb162
jameslee98331 <wkjl2@cam.ac.uk>
2016-10-27 00:55:39 +0100

    updated full program

--- m3fc/phys_sim/accel.py
+++ m3fc/phys_sim/accel.py
@@ -1,25 +1,25 @@
 #Motion parameters
 g = -9.81		#acceleration due to gravity assume to be constant for now
-y =0			#define ground level 	
+y = 0			#define ground level 	
 t=0			#define initial time
 dt=0.001		#small increment of time in 1ms
 v=0			#initial velocity
 Ymax=0			#Register maximum height
 
 #Drag parameters
-SA_roc = 0.031 		#Surface Area of rocket
-SA_chute1=0.031*5	#Surface Area of small parachute
-SA_main=0.031*50	#Surface Area of main parachute
+SA_rock = 0.003	 	#Surface Area of rocket
+SA_drogue= 0.1		#Surface Area of small parachute
+SA_main= 10		#Surface Area of main parachute
 rho = 1.225 		#air density assume to be constant for now
-Cd_rock = 0.5 		#Coefficient of drag of rocket
-Cd_chute1=1.75		#Coefficient of drag of small parachute
-Cd_main=1.75		#Coefficient of drag of main parachute
+Cd_rock = 0.2 		#Coefficient of drag of rocket
+Cd_drogue= 0.8		#Coefficient of drag of small parachute
+Cd_main= 0.8		#Coefficient of drag of main parachute
 
 #Engine parameters
-Br = 10			#-dm/dt
-Vex = 1000		#exhaust velocity relative to rocket
-Md = 100		#Mass of empty
-Mw = 200		#Mass of rocket + fuel
+Br = 1			#-dm/dt
+Vex = 2000		#exhaust velocity relative to rocket
+Md = 20			#Mass of empty
+Mw = 40			#Mass of rocket + fuel
 m=Mw			#initial mass of rocket is wet mass
 
 def drag(SA, rho, Cd, v):		#Drag equation
@@ -37,7 +37,7 @@ def thrust(Vex,Br):			#Thrust force
 #powered_ascend
 while y>= 0 and m >=Md:
 	t+=dt
-	a=(thrust(Vex,Br)-grav(y,m)-drag(SA_roc, rho, Cd_rock, v))/m
+	a=(thrust(Vex,Br)-grav(y,m)-drag(SA_rock, rho, Cd_rock, v))/m
 	v=v+a*dt
 	y=y+v*dt
 	m=m-Br*dt
@@ -45,9 +45,9 @@ while y>= 0 and m >=Md:
 	print(t,v,a,y,m)
 
 #burnout
-while m<=Md and v>=0:
+while m<=Md and v>=0:							#motion under gravity(and drag)
 	t+=dt
-	a=(-grav(y,m)-drag(SA_roc, rho, Cd_rock, v))/m
+	a=(-grav(y,m)-drag(SA_rock, rho, Cd_rock, v))/m
 	v=v+a*dt
 	y=y+v*dt
 	print(t,v,a,y,m)
@@ -57,14 +57,14 @@ while m<=Md and v>=0:
 #drogue_decend
 while v<=0 and y>=300:
 	t+=dt
-	a=(-grav(y,m)+drag(SA_chute1, rho, Cd_chute1, v))/m
+	a=(-grav(y,m)+drag(SA_drogue, rho, Cd_drogue, v))/m		#small parachute is released
 	v=v+a*dt
 	y=y+v*dt
 	print(t,v,a,y,m)
 #main
 while y<=300 and y>=0:
 	t+=dt
-	a=(-grav(y,m)+drag(SA_main, rho, Cd_main, v))/m
+	a=(-grav(y,m)+drag(SA_main, rho, Cd_main, v))/m			#main parachute is released
 	v=v+a*dt
 	y=y+v*dt
 	print(t,v,a,y,m)
