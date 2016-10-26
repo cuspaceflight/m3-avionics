4a269b9fdb00240a722d1a393409600ce5da8aa2
jameslee98331 <wkjl2@cam.ac.uk>
2016-10-26 22:58:09 +0100

    accel update

--- m3fc/phys_sim/accel.py
+++ m3fc/phys_sim/accel.py
@@ -1,13 +1,17 @@
 #Motion parameters
-g = -9.8
+g = -9.81		#acceleration due to gravity assume to be constant for now
 
 #Drag parameters
-SA_roc = 0.031
-air_density = 1.225
-Cd = ?
+SA_roc = 0.031 		#Surface Area of rocket
+rho = 1.225 		#air density assume to be constant for now
+Cd = ? 			#Coefficient of drag of rocket
+
+def drag(SA_roc, rho, Cd, v)		#Drag equation
+	F=0.5*air_density*Cd*A*v**2	
+	return F
 
 #Engine parameters
-burn_rate = ?
-Exhaust_velo = ?
-Mass_dry = ?
-Mass_wet = ?
+Br = ?		#-dm/dt
+Ev = ?		#exhaust velocity relative to rocket
+Md = ?		#Mass of empty
+Mw = ?		#Mass of rocket + fuel
