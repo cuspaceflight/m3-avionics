8c8bf0ca60b649a92a00fed0cdd1636a03d8121c
JMW95 <jamie.wood70@ntlworld.com>
2016-11-18 16:23:10 +0000

    Remove exception when port not found

--- gcs/m3gcs/__init__.py
+++ gcs/m3gcs/__init__.py
@@ -17,14 +17,15 @@ def run():
                         default="/dev/serial/by-id/*m3debug*-if02")
     args = parser.parse_args()
 
-    if "*" in args.port:
-        port = glob.glob(args.port)
-        if len(port) == 0:
-            raise RuntimeError("No port matching glob found")
-        args.port = os.path.realpath(port[0])
-    
     shared_mgr = multiprocessing.Manager()
     global_state = shared_mgr.dict({c:{"data":{}, "time":{}} for c in registered_packets.keys()})
 
-    command_processor.run(port=args.port, state=global_state)
+    if "*" in args.port:
+        port = glob.glob(args.port)
+        if len(port) == 0:
+            print("No port matching glob found, running with no data")
+        else:
+            args.port = os.path.realpath(port[0])
+            command_processor.run(port=args.port, state=global_state)
+
     webapp.run(state=global_state)
