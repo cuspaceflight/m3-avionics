ac8f94a2eba53322ae2754c52b632ba326864bb8
Adam Greig <adam@adamgreig.com>
2016-12-04 00:30:36 +0000

    m3fc config/flash updates

--- m3fc/firmware/m3fc_flash.c
+++ m3fc/firmware/m3fc_flash.c
@@ -10,13 +10,14 @@ static inline bool m3fc_flash_wait_write(void);
 
 void m3fc_flash_write(uint32_t* src, uint32_t* dst, size_t n)
 {
-    chDbgAssert(dst >= (uint32_t*)0x080d9400 && dst < (uint32_t*)0x08100000,
-                "dst must be in flash");
+    chDbgAssert(dst >= (uint32_t*)0x080e0000 && dst < (uint32_t*)0x08100000,
+                "dst must be in flash sector 11");
     chDbgAssert((src >= (uint32_t*)0x20000000 && src < (uint32_t*)0x20020000)
                 ||
                 (src >= (uint32_t*)0x10000000 && src < (uint32_t*)0x10010000),
                 "src must be in ram");
 
+    size_t i;
     uint32_t checksum = m3fc_flash_crc(src, n);
 
     /* Wait for any ongoing flash operations */
@@ -26,30 +27,24 @@ void m3fc_flash_write(uint32_t* src, uint32_t* dst, size_t n)
     FLASH->KEYR = 0x45670123;
     FLASH->KEYR = 0xCDEF89AB;
 
-    /* Configure for 32 bit access */
-    FLASH->CR |= FLASH_CR_PSIZE_1;
-
     /* Erase our configuration sector */
-    FLASH->CR |= FLASH_CR_SER | (M3FC_FLASH_CFG_SECTOR<<3);
+    FLASH->CR = FLASH_CR_SER | (M3FC_FLASH_CFG_SECTOR<<3);
     FLASH->CR |= FLASH_CR_STRT;
 
     /* Wait for erase completion */
     while(FLASH->SR & FLASH_SR_BSY);
 
-    /* Clear SER */
-    FLASH->CR &= ~FLASH_CR_SER;
-
-    /* Write flash */
-    FLASH->CR |= FLASH_CR_PG;
-    while(n--) {
-        *dst++ = *src++;
+    /* Write flash in 32bit chunks */
+    FLASH->CR = FLASH_CR_PG | FLASH_CR_PSIZE_1;
+    for(i=0; i<n; i++) {
+        dst[i] = src[i];
         if(!m3fc_flash_wait_write()) {
             break;
         }
     }
 
     /* Write checksum */
-    *dst = checksum;
+    dst[n] = checksum;
     m3fc_flash_wait_write();
 
     /* Re-lock flash */
@@ -57,17 +52,17 @@ void m3fc_flash_write(uint32_t* src, uint32_t* dst, size_t n)
 }
 
 bool m3fc_flash_read(uint32_t* src, uint32_t* dst, size_t n) {
-    chDbgAssert(src >= (uint32_t*)0x080d9400 && src < (uint32_t*)0x08100000,
-                "src must be in flash");
+    chDbgAssert(src >= (uint32_t*)0x080e0000 && src < (uint32_t*)0x08100000,
+                "src must be in flash sector 11");
     chDbgAssert((dst >= (uint32_t*)0x20000000 && dst < (uint32_t*)0x20020000)
                 ||
                 (dst >= (uint32_t*)0x10000000 && dst < (uint32_t*)0x10010000),
                 "dst must be in ram");
     size_t i;
-    uint32_t flash_crc = m3fc_flash_crc(src, n);
     for(i=0; i<n; i++) {
-        *(dst + i) = *(src + i);
+        dst[i] = src[i];
     }
+    uint32_t flash_crc = src[n];
     uint32_t ram_crc = m3fc_flash_crc(dst, n);
     if(ram_crc == flash_crc) {
         return true;
@@ -79,10 +74,11 @@ bool m3fc_flash_read(uint32_t* src, uint32_t* dst, size_t n) {
 
 static uint32_t m3fc_flash_crc(uint32_t* src, size_t n) {
     uint32_t crc;
+    size_t i;
     RCC->AHB1ENR |= RCC_AHB1ENR_CRCEN;
     CRC->CR |= CRC_CR_RESET;
-    while(n--) {
-        CRC->DR = *src++;
+    for(i=0; i<n; i++) {
+        CRC->DR = src[i];
     }
     crc = CRC->DR;
     RCC->AHB1ENR &= ~RCC_AHB1ENR_CRCEN;
