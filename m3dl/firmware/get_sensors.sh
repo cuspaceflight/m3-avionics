e038703d7da2451b2a068550358b6eff4e4b67a0
mattcoates <mc955@cam.ac.uk>
2016-12-15 00:56:28 +0000

    Updated sensor script

--- m3dl/firmware/get_sensors.sh
+++ m3dl/firmware/get_sensors.sh
@@ -8,19 +8,19 @@ arm-none-eabi-gdb --batch --quiet \
                     -ex 'run' \
                     -ex 'printf "\n\nTEMPERATURE READINGS:  "' \
                     -ex 'printf "CJ=%.001fC ", (double)(((temp_results[77] << 16) | (temp_results[78] << 8) | (temp_results[79])) / 1024)' \
-                    -ex 'printf "T2=%.001f C ", (double)(((TEMP_1_2[5] << 16) | (TEMP_1_2[6] << 8) | (TEMP_1_2[7])) / 1024)' \
-                    -ex 'printf "T4=%.001f C ", (double)(((TEMP_3_4[5] << 16) | (TEMP_3_4[6] << 8) | (TEMP_3_4[7])) / 1024)' \
-                    -ex 'printf "T5=%.001f C ", (double)(((TEMP_5_6[1] << 16) | (TEMP_5_6[2] << 8) | (TEMP_5_6[3])) / 1024)' \
-                    -ex 'printf "T6=%.001f C ", (double)(((TEMP_5_6[5] << 16) | (TEMP_5_6[6] << 8) | (TEMP_5_6[7])) / 1024)' \
-                    -ex 'printf "T7=%.001f C ", (double)(((TEMP_7_8[1] << 16) | (TEMP_7_8[2] << 8) | (TEMP_7_8[3])) / 1024)' \
-                    -ex 'printf "T8=%.001f C\n\n", (double)(((TEMP_7_8[5] << 16) | (TEMP_7_8[6] << 8) | (TEMP_7_8[7])) / 1024)' \
+                    -ex 'printf "T2=%.01fC ", (double)(((TEMP_1_2[5] << 16) | (TEMP_1_2[6] << 8) | (TEMP_1_2[7])) / 1024)' \
+                    -ex 'printf "T4=%.01fC ", (double)(((TEMP_3_4[5] << 16) | (TEMP_3_4[6] << 8) | (TEMP_3_4[7])) / 1024)' \
+                    -ex 'printf "T5=%.01fC ", (double)(((TEMP_5_6[1] << 16) | (TEMP_5_6[2] << 8) | (TEMP_5_6[3])) / 1024)' \
+                    -ex 'printf "T6=%.01fC ", (double)(((TEMP_5_6[5] << 16) | (TEMP_5_6[6] << 8) | (TEMP_5_6[7])) / 1024)' \
+                    -ex 'printf "T7=%.01fC ", (double)(((TEMP_7_8[1] << 16) | (TEMP_7_8[2] << 8) | (TEMP_7_8[3])) / 1024)' \
+                    -ex 'printf "T8=%.01fC\n\n", (double)(((TEMP_7_8[5] << 16) | (TEMP_7_8[6] << 8) | (TEMP_7_8[7])) / 1024)' \
                     -ex 'delete 1' \
                     -ex 'break pressure.c:112'  \
                     -ex 'continue' \
                     -ex 'printf "\n\nPRESSURE READINGS:  "' \
-                    -ex 'printf "P1=%.01f kPa ", (double)((res[1] << 8) | res[0]) * 1.25'\
-                    -ex 'printf "P2=%.01f kPa ", (double)((res[3] << 8) | res[2])  * 1.25'\
-                    -ex 'printf "P3=%.01f kPa ", (double)((res[5] << 8) | res[4])  * 1.25'\
-                    -ex 'printf "P4=%.01f kPa\n\n", (double)((res[7] << 8) | res[6])  * 1.25'\
+                    -ex 'printf "P1=%.01fkPa ", (double)((res[1] << 8) | res[0]) * 1.25'\
+                    -ex 'printf "P2=%.01fkPa ", (double)((res[3] << 8) | res[2])  * 1.25'\
+                    -ex 'printf "P3=%.01fkPa ", (double)((res[5] << 8) | res[4])  * 1.25'\
+                    -ex 'printf "P4=%.01fkPa\n\n", (double)((res[7] << 8) | res[6])  * 1.25'\
                     
 
