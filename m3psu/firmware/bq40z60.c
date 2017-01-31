2d24829c727ae18917cd5aef284e302e6b9a5dbc
Jamie Wood <jamie.wood70@ntlworld.com>
2017-01-31 14:34:13 +0000

    List known setup for charger registers

--- m3psu/firmware/bq40z60.c
+++ m3psu/firmware/bq40z60.c
@@ -9,65 +9,108 @@
 #include "smbus.h"
 #include "bq40z60.h"
 
-/////////// CONFIGURE THESE IN DATA MEMORY
-// Disable temperature sensing
-// Set charging voltage
-// Set charging current
+//////// PROTECTIONS CONFIG ////////
 
-//// Sbs Configuration
-// BCAST -> 0
-//// FET Options
-// CHGFET -> 1
-//// Mfg Status Init
-// FET_EN -> 1
-// LED_EN -> 0 (default)
-// CHGR_EN -> 0 (default)
-// GAUGE_EN -> 1
-//// DA Configuration
-// NR (non-removable) -> 1
-// CC (cell count) -> 1 (value is num cells - 1)
-//// Temperature Enable
+//// Settings:Protection:Enabled Protections B
+// OTC -> 0 (disable charge overtemperature protection)
+// OTD -> 0 (disable discharge overtemperature protection)
+
+//// Settings:Protection:Enabled Protections C
+// OTF -> 0 (Disable FET overtemperature protection)
+
+//// Settings:Protection:Enabled Protections D
+// UTC -> 0 (disable charge undertemperature protection)
+// UTD -> 0 (disable discharge undertemperature protection)
+
+//// Settings:Configuration:Temperature Enable
 // TS{0-4} Enable -> 0 (Disabled)
 // TSInt Enable -> 1 (Enabled)
-//// Temperature Mode
-// TSInt Mode -> 1 (FET)
-//// Balancing Configuration
-// CB -> 1 (Enable balancing)
-// CBR -> 1 (Balancing at rest)
-// CBM -> 0 (Internal balancing) (*** CHECK ***)
-// Min Start Balance Delta -> ?
-// Relax Balance Interval -> ?
-// Min RSOC for Balancing -> ?
-// (See section 8.5 to configure balancing at rest)
-//// Charger Setup
-// Minimum Voltage Output -> 4192 (610 * (1 + (330k / 56.2k)))
-// Voltage Resolution -> 16 ((610 * (1 + (330k / 56.2k))) / 256)
-// Current Resolution -> 39 (0.39 / 0.01ohm)
-// Max Current Register -> ?? (ILimit * 0.01ohm * 2550)
-// 
 
-//// Impedence track config:
-// Load Mode -> 1 (Constant Power)
-// Load Select -> 7 (Max Avg P Last Run) (apparently best for variable loads)
-// Term Voltage ?
-// Term Min Cell V ?
-// ReserveCap-mWh ?
-// QMax Initial Values ?
-// Design Capacity -> 2200
-// Design Voltage -> 7400
-// Update Status ?
+//// Settings:Configuration:Temperature Mode
+// TSInt Mode -> 0 (CELL)
 
-//// Alert Interrupt
-// BTP_EN -> 1
-// BTP_POL -> 
+
+/////// CHARGER SETTINGS ////////
+
+// Settings:Configuration:Mfg Status Init
+// CHGR_EN -> 0 (default)
+
+// Advanced Charge Algorithm:Charger:Minimum Voltage Output -> 4192 (610 * (1 + (330k / 56.2k)))
+// Advanced Charge Algorithm:Charger:Voltage Resolution -> 16 ((610 * (1 + (330k / 56.2k))) / 256)
+// Advanced Charge Algorithm:Charger:Current Resolution -> 39 (0.39 / 0.01ohm)
+// Advanced Charge Algorithm:Charger:Max Current Register -> 255 (default) (ILimit * 0.01ohm * 2550)
+
+//// Settings:Configuration:DA Configuration
+// CC (cell count) -> 1 (value is num cells - 1)
+
+// Advanced Charge Algorithm:Standard Temp Charging:Current Low -> 990 mA
+// Advanced Charge Algorithm:Standard Temp Charging:Current Med -> 1000 mA
+// Advanced Charge Algorithm:Standard Temp Charging:Current High -> 1000 mA
+
+// Advanced Charge Algorithm:Rec Temp Charging:Current Low -> 1000 mA
+// Advanced Charge Algorithm:Rec Temp Charging:Current Med -> 1000 mA
+// Advanced Charge Algorithm:Rec Temp Charging:Current High -> 1000 mA
+
+// Advanced Charge Algorithm:High Temp Charging:Current Low -> 506 mA
+// Advanced Charge Algorithm:High Temp Charging:Current Med -> 990 mA
+// Advanced Charge Algorithm:High Temp Charging:Current High -> 748 mA
+
+
+//////// IO CONFIG ////////
+
+//// Settings:Manufacturing:Mfg Status Init
+// LED_EN -> 0 (default)
+// FET_EN -> 1 (allow enabling of FETs)
+
+//// Settings:Configuration:DA Configuration
+// NR -> 1 (non-removable)
+
+
+//////// GAUGING CONFIG ////////
+
+//// Settings:Manufacturing:Mfg Status Init
+// GAUGE_EN -> 1
+
+// Gas Gauging:State:Qmax Cell 1 -> 2200 mAh
+// Gas Gauging:State:Qmax Cell 2 -> 2200 mAh
+// Gas Gauging:State:Qmax Cell 3 -> 2200 mAh
+// Gas Gauging:State:Qmax Cell 4 -> 2200 mAh
+// Gas Gauging:State:Qmax Pack -> 2200 mAh
+
+// Gas Gauging:Design:Design Capacity -> 2000mAh
+// Gas Gauging:Design:Design Voltage -> 7400 mV
+
+// Gas Gauging:IT Cfg:Load Mode -> 1 (Constant Power)
+// Gas Gauging:IT Cfg:Load Select -> 7 (default) (Max Avg P Last Run) (apparently best for variable loads)
+// Gas Gauging:IT Cfg:Term Voltage -> 6000 mV (2 cells * 3000mV/cell)
+// Gas Gauging:IT Cfg:Update Status ?
+
+//// Settings:Configuration:IO Config
+// BTP_POL -> 0 (default) (assert low)
+// BTP_EN -> 1 (enable BTP interrupt)
+
+//(*** TEST THIS -> MAYBE LEAVE AS 00? ***)
+//// Gas Gauging:State:Update Status
+// Enable -> 1 (IT and gauging enabled)
+// UPDATE -> 1 (QMax updates allowed, but not Ra table updates)
+
+
+//////// BALANCING CONFIG ////////
+
+//// Settings:Configuration:Balancing Configuration
+// CB -> 1 (default) (Enable balancing)
+// CBR -> 0 (default) (Disable balancing at rest)
 
 
 /////////// REGISTERS TO USE
 // MAC DAStatus1 gives cell voltages, power etc.
+// MAC DAStatus2 gives temperatures
 // RelativeStateOfCharge (RSOC)
 // FullChargeCapacity (FCC)
 // RemainingCapacity (RemCap)
-
+// BatteryStatus gives DSG (1 if not charging), other flags (e.g. protections)
+// OperationStatus give XCHG (1 if charging prohibited - see manual 4.12)
+// ITStatus1, ITStatus2, ITStatus3 for gauging info (for debug only?)
 
 #define BQ40Z60_CMD_MANUFACTURER_ACCESS     0x44
 
