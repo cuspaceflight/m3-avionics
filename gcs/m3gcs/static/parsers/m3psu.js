5bd15ee1b2acaac1a914e3d5f0f1695a983abbf7
JMW95 <jamie.wood70@ntlworld.com>
2017-02-13 20:40:50 +0000

    Update m3psu parser for new CAN messages

--- gcs/m3gcs/static/parsers/m3psu.js
+++ gcs/m3gcs/static/parsers/m3psu.js
@@ -9,12 +9,10 @@ var M3PSU = function(gcs){
     var CAN_MSG_ID_M3PSU_CHANNEL_STATUS_910 = CAN_ID_M3PSU | msg_id(53);
     var CAN_MSG_ID_M3PSU_CHANNEL_STATUS_1112= CAN_ID_M3PSU | msg_id(54);
     var CAN_MSG_ID_M3PSU_TOGGLE_CHANNEL = CAN_ID_M3PSU | msg_id(17);
-    var CAN_MSG_ID_M3PSU_INTEXT_STATUS = CAN_ID_M3PSU | msg_id(57);
+    var CAN_MSG_ID_M3PSU_CAPACITY = CAN_ID_M3PSU | msg_id(57);
     var CAN_MSG_ID_M3PSU_PYRO_STATUS = CAN_ID_M3PSU | msg_id(48);
     var CAN_MSG_ID_M3PSU_CHARGER_STATUS = CAN_ID_M3PSU | msg_id(55);
     var CAN_MSG_ID_M3PSU_TOGGLE_CHARGER = CAN_ID_M3PSU | msg_id(18);
-    var CAN_MSG_ID_M3PSU_TOGGLE_BALANCE = CAN_ID_M3PSU | msg_id(19);
-    var CAN_MSG_ID_M3PSU_TOGGLE_INTEXT = CAN_ID_M3PSU | msg_id(20);
 
     this.channels = [];
     for(var i=0; i<12; i++){
@@ -22,37 +20,31 @@ var M3PSU = function(gcs){
     }
 
     this.charger = {
-        should_balance: false,
-        should_charge: false,
-        bleed_batt1: false,
-        bleed_batt2: false,
-        is_charging: false,
-        overcurrent: false,
+        enabled: false,
+        charging: false,
+        inhibit: false,
+        voltage_mode: "low",
+        temperature: 0,
         current: 0,
     };
+    
+    this.runtime = 0;
+    this.percent = 0;
 
     this.cells = [0, 0];
+    this.batt_voltage = 0;
 
     this.pyro = {
         v:0, i:0, p:0,
         enabled: false,
     };
 
-    this.int_enabled = false;
-    this.ext_enabled = false;
-
     var _this = this;
 
     gcs.registerPacket(CAN_MSG_ID_M3PSU_BATT_VOLTAGES, function(data){
-        _this.cells[0] = data[0] * 0.02;
-        _this.cells[1] = data[1] * 0.02;
-
-        var state = data[2];
-
-        var c = _this.charger;
-        c.should_balance = bool(state & 4);
-        c.bleed_batt1 = bool(state & 2);
-        c.bleed_batt2 = bool(state & 1);
+        _this.cells[0] = ((data[1] << 8) | data[0]) * 0.01;
+        _this.cells[1] = ((data[3] << 8) | data[2]) * 0.01;
+        _this.batt_voltage = ((data[5] << 8) | data[4]) * 0.01;
     });
 
     gcs.registerPacket(CAN_MSG_ID_M3PSU_TOGGLE_PYROS, function(data){} );
@@ -103,26 +95,35 @@ var M3PSU = function(gcs){
     });
 
     gcs.registerPacket(CAN_MSG_ID_M3PSU_CHARGER_STATUS, function(data){
-        var current = ((data[1] << 8) | data[0]) / 1000.0;
+        var current = (data[1] << 8) | data[0];
+        if(current > 32767){ // Convert to unsigned
+            current -= 65536;
+        }
         var state = data[2];
+        var tempcK = (data[4] << 8) | data[3];
+        
+        var vmode = (state >> 3) & 0x3;
 
         var c = _this.charger;
         c.current = current;
-        c.should_charge = bool(state & 1);
-        c.is_charging = bool(state & 2);
-        c.overcurrent = bool(state & 4);
+        c.enabled = bool(state & 1);
+        c.charging = bool(state & 2);
+        c.inhibit = bool(state & 4);
+        c.voltage_mode = ["Pre-charge", "Low", "Med", "High", "INVAL"][vmode];
+        c.temperature = (tempcK/10) - 273.2;
+    });
+    
+    gcs.registerPacket(CAN_MSG_ID_M3PSU_CAPACITY, function(data){
+        var mins = ((data[1] << 8) | data[0]);
+        var percent = data[2];
+        
+        if(mins == 65535){ // Charging, so runtime is infinite
+            mins = Infinity;
+        }
+        
+        _this.percent = percent;
+        _this.runtime = mins;
     });
 
     gcs.registerPacket(CAN_MSG_ID_M3PSU_TOGGLE_CHARGER, function(data){} );
-
-    gcs.registerPacket(CAN_MSG_ID_M3PSU_TOGGLE_BALANCE, function(data){} );
-
-    gcs.registerPacket(CAN_MSG_ID_M3PSU_TOGGLE_INTEXT, function(data){} );
-
-    gcs.registerPacket(CAN_MSG_ID_M3PSU_INTEXT_STATUS, function(data){
-        var state = data[0];
-
-        _this.int_enabled = bool(state & 2);
-        _this.ext_enabled = bool(state & 1);
-    });
 }
