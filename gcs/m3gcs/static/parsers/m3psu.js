a66445c813a47283310c498d0297c627b9981f20
JMW95 <jamie.wood70@ntlworld.com>
2017-02-17 19:18:03 +0000

    Update m3psu parser for new messages

--- gcs/m3gcs/static/parsers/m3psu.js
+++ gcs/m3gcs/static/parsers/m3psu.js
@@ -13,6 +13,7 @@ var M3PSU = function(gcs){
     var CAN_MSG_ID_M3PSU_PYRO_STATUS = CAN_ID_M3PSU | msg_id(48);
     var CAN_MSG_ID_M3PSU_CHARGER_STATUS = CAN_ID_M3PSU | msg_id(55);
     var CAN_MSG_ID_M3PSU_TOGGLE_CHARGER = CAN_ID_M3PSU | msg_id(18);
+    var CAN_MSG_ID_M3PSU_AWAKE_TIME = CAN_ID_M3PSU | msg_id(58);
 
     this.channels = [];
     for(var i=0; i<12; i++){
@@ -23,6 +24,8 @@ var M3PSU = function(gcs){
         enabled: false,
         charging: false,
         inhibit: false,
+        battleshort: false,
+        acfet: false,
         voltage_mode: "low",
         temperature: 0,
         current: 0,
@@ -38,6 +41,9 @@ var M3PSU = function(gcs){
         v:0, i:0, p:0,
         enabled: false,
     };
+
+    this.awake_time = 0;
+    this.power_mode = "low";
 
     var _this = this;
 
@@ -84,8 +90,8 @@ var M3PSU = function(gcs){
 
     gcs.registerPacket(CAN_MSG_ID_M3PSU_PYRO_STATUS, function(data){
         var v = ((data[1] << 8) | data[0]) / 1000.0;
-        var i = ((data[3] << 8) | data[2]) / 1000000.0;
-        var p = ((data[5] << 8) | data[4]) / 100000.0;
+        var i = ((data[3] << 8) | data[2]) / 1000.0;
+        var p = ((data[5] << 8) | data[4]) / 1000.0;
         var state = data[6];
 
         _this.pyro.v = v;
@@ -109,6 +115,8 @@ var M3PSU = function(gcs){
         c.enabled = bool(state & 1);
         c.charging = bool(state & 2);
         c.inhibit = bool(state & 4);
+        c.battleshort = bool(state & 32);
+        c.acfet = bool(state & 64);
         c.voltage_mode = ["Pre-charge", "Low", "Med", "High", "INVAL"][vmode];
         c.temperature = (tempcK/10) - 273.2;
     });
@@ -116,13 +124,21 @@ var M3PSU = function(gcs){
     gcs.registerPacket(CAN_MSG_ID_M3PSU_CAPACITY, function(data){
         var mins = ((data[1] << 8) | data[0]);
         var percent = data[2];
-        
+
         if(mins == 65535){ // Charging, so runtime is infinite
             mins = Infinity;
         }
-        
+
         _this.percent = percent;
         _this.runtime = mins;
+    });
+
+    gcs.registerPacket(CAN_MSG_ID_M3PSU_AWAKE_TIME, function(data){
+        var secs = ((data[1] << 8) | data[0]);
+        var status = data[2];
+
+        _this.awake_time = secs;
+        _this.power_mode = (status & 1) ? "low" : "high";
     });
 
     gcs.registerPacket(CAN_MSG_ID_M3PSU_TOGGLE_CHARGER, function(data){} );
