f1e5a262df65be42c3c63047f1749c0b16c0f5ea
mattcoates <mc955@cam.ac.uk>
2017-06-30 21:22:28 +0100

    generated full board.h and configured chibios settings and peripheral and dma settings

---
+++ TOAD/firmware/main.c
@@ -0,0 +1,15 @@
+#include "ch.h"
+#include "hal.h"
+
+int main(void) {
+
+    /* Allow debug access during WFI sleep */
+    DBGMCU->CR |= DBGMCU_CR_DBG_SLEEP;
+
+    /* Initialise ChibiOS */
+    halInit();
+    chSysInit();
+    
+    /* Do some stuff */
+
+}
