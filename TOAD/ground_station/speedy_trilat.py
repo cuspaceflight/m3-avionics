5889ab3c7efbd425b5ec1e4b3e3d6432f556fb61
Greg <gregox273@gmail.com>
2017-07-05 23:02:13 +0100

    Create toad_gcs branch and begin porting trilateration algorithm to python

---
+++ TOAD/ground_station/speedy_trilat.py
@@ -0,0 +1,21 @@
+"""
+Speedy trilateration algorithm
+
+#INSERT USAGE SYNTAX
+
+Derivation: Y. Zhou, "An Efficient Least-Squares Trilateration Algorithm for Mobile Robot Localization"
+
+Matlab implementations in TOAD documentation
+"""
+import sys
+
+def speedy_trilat(p_i, r_i, n=3):
+    """ Returns position estimate from ground station locations and distances between them and tracked objects """
+    # p_i is nxN matrix containing position vectors of reference points
+    #r_i is N element vector containing distances from each reference
+    #n is number of spatial dimensions (2 or 3)
+    
+
+    return pos
+
+
