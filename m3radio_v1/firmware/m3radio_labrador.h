3fabd4c848d0a34f6cfc7e6625f8967ff03054d0
Adam Greig <adam@adamgreig.com>
2017-07-21 01:27:08 +0100

    remove old m3radio_v1 directory

--- m3radio_v1/firmware/m3radio_labrador.h
+++
@@ -1,9 +0,0 @@
-#ifndef M3RADIO_LABRADOR_H
-#define M3RADIO_LABRADOR_H
-
-#define M3RADIO_LABRADOR_TXBUFSIZE (128)
-
-void m3radio_labrador_init(void);
-void m3radio_labrador_tx(uint8_t* buf);
-
-#endif
--- m3radio_v1/lab01_gc_firmware/usbserial.h
+++
@@ -1,7 +0,0 @@
-#ifndef USBSERIAL_H
-#define USBSERIAL_H
-
-void usbserial_init(void);
-void usbserial_send(uint16_t sid, uint8_t rtr, uint8_t* data, uint8_t dlc);
-
-#endif
