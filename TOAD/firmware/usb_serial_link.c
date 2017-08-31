58eea457a96fabd516feeaf490083d81025f24d5
mattcoates <mc955@cam.ac.uk>
2017-08-31 04:37:17 +0100

    need to change usb pins

--- TOAD/firmware/usb_serial_link.c
+++ TOAD/firmware/usb_serial_link.c
@@ -17,10 +17,10 @@ static THD_FUNCTION(USBThread, arg) {
     usbDisconnectBus(serusbcfg.usbp);
     chThdSleepMilliseconds(1500);
     usbStart(serusbcfg.usbp, &usbcfg);
-    usbConnectBus(serusbcfg.usbp);
+    //usbConnectBus(serusbcfg.usbp);
     
     while(true) {
-    chThdSleepMilliseconds(1500);
+        chThdSleepMilliseconds(1500);
     }
     
 }
